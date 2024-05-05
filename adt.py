38ec4e06c72dd6c95f718b776e09de2359a77738
hellspawn17 <118900919+hellspawn17@users.noreply.github.com>
2024-05-06 02:17:28 +0530

    Update adt.py

--- adt.py
+++ adt.py
@@ -1,3 +1,7 @@
+#Implement all the functions of a dictionary (ADT) using hashing and
+#handle collisions using chaining with / without replacement. Data: Set of
+#(key, value)pairs, Keys are mapped to values, Keys must be comparable,
+#Keys must be unique Standard Operations: Insert (key, value), Find(key), Delete(key)(python)
 class HashFunction:
     def __init__(self):
         self.h = [{'key': -1, 'name': 'NULL'} for _ in range(10)]
